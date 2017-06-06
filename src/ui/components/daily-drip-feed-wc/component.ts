7202bfdfecf807e8ccdc42aedd8c0e21710ef1bc
Brandon Blaylock <brandon.blaylock@gmail.com>
2017-06-06 10:56:12 -0500

    simple glimmer component

--- src/ui/components/daily-drip-feed-wc/component.ts
+++ src/ui/components/daily-drip-feed-wc/component.ts
@@ -1,5 +1,13 @@
-import Component from "@glimmer/component";
+import Component, {tracked} from "@glimmer/component";
 
 export default class DailyDripFeedWc extends Component {
-
+  @tracked results;
+  constructor(options) {
+    super(options);
+    this.getFeed();
+  };
+  async getFeed(){
+    let jsonFeed = await fetch('http://rss2json.com/api.json?rss_url=https%3A%2F%2Fwww.dailydrip.com%2Ftopics%2Fember%2Ffeed.rss');
+    this.results = await jsonFeed.json();
+  };
 }
